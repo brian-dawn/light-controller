9505ca5a4538a0e09781aeb7164b52962189dd00
Brian Dawn <brian.t.dawn@gmail.com>
2022-06-09 20:08:56 -0500

    Change the transition time to 30 minutes.

--- lightcontroller/app.py
+++ lightcontroller/app.py
@@ -46,7 +46,7 @@ print("sunrise: " + str(sunrise))
 print("sunset: " + str(sunset))
 
 
-transition_seconds = 60 * 10  # 10 minutes
+transition_seconds = 60 * 30  # 30 minutes
 orange_duration = 60 * 60 * 2  # 2 hours, we turn orange at night to prepare for sleep.
 
 # When a user presses a button we will stop doing sun updates.
